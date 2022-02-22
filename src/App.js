2e2552ae9473bd4a4e19d627714e290a3a3e2af3
Igor Santos <igorsantos9513@outlook.com>
2022-02-22 15:07:51 -0300

    first release

--- src/App.js
+++ src/App.js
@@ -1,67 +1,22 @@
-import { useState, useEffect } from "react";
-import api from "./services/api";
-import Card from "./components/Card";
-import './app.css';
-import Shop from "./components/Shop";
-
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
-  const [data, setData] = useState([])
-  const [cartItems, setCartItems] = useState([])
-
-    useEffect(()=>{
-        api
-          .get('api/fruit/all')
-          .then((response) => setData(response.data))
-          .catch((err)=>{
-            console.log("Error: " + err);
-          })
-      }, []);
-
-      console.log(data)
-
-      const fruits = data.splice(0, 22);
-
-      const onAdd = (props) => {
-        const exist = cartItems.find((x) => x.id === props.id);
-    if (exist) {
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === props.id ? { ...exist, qty: exist.qty + 1 } : x
-        )
-      );
-    } else {
-      setCartItems([...cartItems, { ...props, qty: 1 }]);
-    }
-      };
-
-
   return (
-    <>
-      <div className='container'>
-        {fruits.map((item)=>(
-          <div key={item.id}>
-            <Card
-              name={item.name}
-              carbohydrates={item.nutritions.carbohydrates}
-              protein={item.nutritions.protein}
-              fat={item.nutritions.fat}
-              calories={item.nutritions.calories}
-              sugar={item.nutritions.sugar}
-              onAdd={onAdd}
-              
-            />
-
-          </div>
-            
-
-        ))}
-          
-          <Shop  onAdd={onAdd} cartItems={cartItems}/>
-          
-
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Card from './components/Card'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Context from "./context/Context";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Context>
+      <App />
+    </Context>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
